5590c594b9e473c809b2e27ae077f8d2f8e792ab
magic-oreo <deh.vud@gmail.com>
2023-03-07 17:36:55 -0600

    1.13: Add array for votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,13 +1,8 @@
 import { useState } from 'react'
 
 const App = () => {
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-
-  const anecdotes = [
+  const anecdotes =
+  [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -17,6 +12,30 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  const count = anecdotes.length - 1
+
+  const Anecdotes = ({ anecdotes, points }) => {
+    return (
+      <>
+        {anecdotes[selected]}<br />
+        has {points[selected]} votes <br />
+      </>
+    )
+  }
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+  const vote = (selected) => {
+    const newPoints = [...points]
+    newPoints[selected] += 1
+    setPoints(newPoints)
+  }
 
   const selectRandom = (count) => {
     const handler = Math.floor(Math.random() * count)
@@ -24,14 +43,12 @@ const App = () => {
     return (handler)
   }
 
-  const [selected, setSelected] = useState(0)
-  const count = anecdotes.length - 1
-
   return (
-    <div>
-      {anecdotes[selected]}<br />
+    <>
+      <Anecdotes anecdotes={anecdotes} points={points} />
+      <Button handleClick={() => vote(selected)} text="vote" />
       <Button handleClick={() => setSelected(selectRandom(count))} text="next anecdote" />
-    </div>
+    </>
   )
 }
 
