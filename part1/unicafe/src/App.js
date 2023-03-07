75f31fc5fee45033c97d06a4663bced2c756fc76
magic-oreo <deh.vud@gmail.com>
2023-03-07 15:51:20 -0600

    1.10: Refactor

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,47 +1,62 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+  const Content = () => {
+    return (
+      <>
+        <Button handleClick={() => setGood(good + 1)} text='good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      </>
+    )
+  }
+
   const Statistics = ({ good, neutral, bad }) => {
-    const total = (good + neutral + bad)
-    const average = (good-bad)/total
-    const positive = (good/total)*100
-  
-    if(total === 0) return (
-    <div>
-      <h1>statistics</h1>
-      No feedback given
-      </div>
+    const all = (good + neutral + bad)
+    const average = (good - bad) / all
+    const positive = (good / all) * 100 + '%'
+
+    const StatisticLine = ({ text, value }) => {
+      return (
+        <>
+          {text} {value} <br />
+        </>
+      )
+    }
+
+    if (all === 0) return (
+      <>
+        No feedback given
+      </>
     )
     return (
-      <div>
-        <h1>statistics</h1>
-        good {good} <br />
-        neutral {neutral} <br />
-        bad {bad} <br />
-        all {total} <br />
-        average {average} <br />
-        positive {positive}%
-      </div>
+      <>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </>
     )
   }
 
   return (
     <>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Content />
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
