14aae62b627af2af863b0d6ce3ee9467cb449dfe
magic-oreo <deh.vud@gmail.com>
2023-03-07 15:24:22 -0600

    1.8: Refactored Statistics within App component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,30 +6,29 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-
-const Statistics = ({ good, neutral, bad }) => {
-  const total = (good + neutral + bad)
-  const average = (good-bad)/total
-  const positive = (good/total)*100
-
-  return (
-    <div>
-      <h1>statistics</h1>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad} <br />
-      all {total} <br />
-      average {average} <br />
-      positive {positive}%
-    </div>
-  )
-}
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const Statistics = ({ good, neutral, bad }) => {
+    const total = (good + neutral + bad)
+    const average = (good-bad)/total
+    const positive = (good/total)*100
+  
+    return (
+      <div>
+        <h1>statistics</h1>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad} <br />
+        all {total} <br />
+        average {average} <br />
+        positive {positive}%
+      </div>
+    )
+  }
 
   return (
     <>
