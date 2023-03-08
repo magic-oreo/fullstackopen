de1d740cdb262197eb0cb098553dca1680a20bd1
magic-oreo <deh.vud@gmail.com>
2023-03-08 13:38:35 -0600

    Format

--- part2/courseinfo/src/modules/Course.js
+++ part2/courseinfo/src/modules/Course.js
@@ -1,43 +1,41 @@
 const Header = ({ name }) => {
     return <h1>{name}</h1>
-  }
-  
-  const Content = ({ parts }) => {
-    return (
-      <>
-        {parts.map(parts =>
-          <Part key={parts.id} part={parts} />
-        )}
-      </>
-    )
-  }
-  
-  const Part = ({ part }) => {
-    return (
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    )
-  }
-  
-  const Total = ({ sum }) => {
-    return (
-      <p><b>Total of {sum} exercises</b></p>
-    )
-  }
-  
-  const Course = ({ course }) => {
-    const sum = course.parts
-      .map((part) => part.exercises)
-      .reduce((prev, cur) => prev + cur)
-  
-    return (
-      <>
-        <Header name={course.name} />
-        <Content parts={course.parts} />
-        <Total sum={sum} />
-      </>
-    )
-  }
+}
 
-  export default Course
+const Content = ({ parts }) => {
+    return (
+        <>
+            {parts.map(parts =>
+                <Part key={parts.id} part={parts} />
+            )}
+        </>
+    )
+}
+
+const Part = ({ part }) => {
+    return (
+        <p>{part.name} {part.exercises}</p>
+    )
+}
+
+const Total = ({ sum }) => {
+    return (
+        <p><b>Total of {sum} exercises</b></p>
+    )
+}
+
+const Course = ({ course }) => {
+    const sum = course.parts
+        .map((part) => part.exercises)
+        .reduce((prev, cur) => prev + cur)
+
+    return (
+        <>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total sum={sum} />
+        </>
+    )
+}
+
+export default Course
