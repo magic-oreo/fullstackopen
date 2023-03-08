10e19f509ed883bf7cf5e5bfe8fc09843b0b03b7
magic-oreo <deh.vud@gmail.com>
2023-03-08 13:30:25 -0600

    2.4: Extend course to array

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -28,8 +28,8 @@ const Total = ({ sum }) => {
 
 const Course = ({ course }) => {
   const sum = course.parts
-  .map((part) => part.exercises)
-  .reduce((prev, cur) => prev + cur)
+    .map((part) => part.exercises)
+    .reduce((prev, cur) => prev + cur)
 
   return (
     <>
@@ -41,34 +41,59 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'The final frontier',
-        exercises: 58,
-        id: 4
-      }
-    ]
-  }
+  const course = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'The final frontier',
+          exercises: 58,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <>
+    {course.map(course => 
+    <Course key={course.id} course={course} />
+      )}
+</>
+      
+  )
 }
 
 export default App
