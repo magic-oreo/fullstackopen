3355bf6db12c70507177ddc84400ec848c9e45a7
magic-oreo <deh.vud@gmail.com>
2023-03-08 16:16:41 -0600

    2.9: Implement name filtering

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '401-44567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,9 +37,19 @@ const App = () => {
     )
   }
 
+const personsToShow = persons.filter(person => person.name.toLowerCase().includes(newFilter))
+
+const handleFilter = (event) => {
+  setNewFilter(event.target.value)
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with <input value={newFilter} onChange={handleFilter} />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
@@ -49,8 +62,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
-          <Person key={person.name} person={person} />
+      {personsToShow.map(person =>
+          <Person key={person.id} person={person} />
         )}
     </div>
   )
