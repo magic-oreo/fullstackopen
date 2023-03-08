481eb8600759a72bd5a695eff3c0d4a215238786
magic-oreo <deh.vud@gmail.com>
2023-03-08 14:55:22 -0600

    2.6: Display names in array

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,22 +3,46 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const Name = ({ person }) => {
+    return (
+      <>
+      {person.name}<br />
+      </>
+    )
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+          <Name key={person.name} person={person} />
+        )}
     </div>
   )
 }
