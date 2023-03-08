f598489d0ab301be3990bca9e04850f54e7d94ad
magic-oreo <deh.vud@gmail.com>
2023-03-08 15:50:14 -0600

    2.8: Implement phone numbers

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,33 +2,34 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '401-44567' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if(persons.some(({name}) => name === newName))
     {
       alert(`${newName} is already added to phonebook`)
       setNewName('')
+      setNewNumber('')
       return
     }
     setPersons(persons.concat(nameObject))
-
+    setNewName('')
+    setNewNumber('')
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const Name = ({ person }) => {
+  const Person = ({ person }) => {
     return (
       <>
-      {person.name}<br />
+      {person.name} {person.number}<br />
       </>
     )
   }
@@ -38,7 +39,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          number: <input type="tel" value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -46,7 +50,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
-          <Name key={person.name} person={person} />
+          <Person key={person.name} person={person} />
         )}
     </div>
   )
