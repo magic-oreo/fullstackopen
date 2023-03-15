f37ae12aa0f0da91ce7eeb90b9737ae8b8e60498
magic-oreo <deh.vud@gmail.com>
2023-03-15 17:23:31 -0500

    3.10: Deploy backend

---
+++ part3/phonebook-backend/README.md
@@ -0,0 +1,1 @@
+https://magicoreo-phonebook.fly.dev/
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('request', (request) => {
   return JSON.stringify(request.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request'))
 
@@ -87,7 +89,12 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
