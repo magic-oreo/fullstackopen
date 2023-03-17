546ed7a82fb74ba1d5996a6ab6b3191de364b41c
magic-oreo <deh.vud@gmail.com>
2023-03-17 16:34:44 -0500

    4.5: favoriteBlog test function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,8 +8,22 @@ const totalLikes = (blogs) => {
     : blogs.map(blog => blog.likes).reduce((a,b) => a+b)
 }
 
+const favoriteBlog = (blogs) => {
+    const favorite = () => {
+        const favorite = blogs.find(blog => blog.likes === Math.max(...blogs.map(blog => blog.likes)))
+        delete favorite._id
+        delete favorite.__v
+        delete favorite.url
+        return favorite
+    }
+    return blogs === undefined
+    ? undefined
+    : favorite()
+}
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
