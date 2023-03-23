97da3e0e2c346da344a99ac507c3c241b0bb07cf
magic-oreo <deh.vud@gmail.com>
2023-03-23 16:29:13 -0500

    4.15: Implement userId in blog router

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,16 +1,30 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blog = await Blog.find({})
+  const blog = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blog)
 })
 
 blogRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findById(body.userId)
 
-  const result = await blog.save()
-  response.status(201).json(result)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
 })
 
 blogRouter.delete('/:id', async (request, response) => {
