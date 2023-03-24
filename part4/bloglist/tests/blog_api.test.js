696176e8473f57867c89b9d8567bd238ad22939b
magic-oreo <deh.vud@gmail.com>
2023-03-24 16:54:23 -0500

    4.23: Fix broken tests and add test for invalid token

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,16 +2,23 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
 beforeEach(async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
+
+  await User.deleteMany({})
+  const passwordHash = await bcrypt.hash('sekret', 10)
+  const user = new User({ username: 'root', passwordHash })
+  await user.save()
 })
 
-describe('when there is initially some notes saved', () => {
+describe('when there is initially some blogs saved', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -38,10 +45,12 @@ describe('when there is initially some notes saved', () => {
     expect(response.id).toBeDefined()
   })
 })
-describe('addition of a new note', () => {
+describe('addition of a new blog', () => {
   test('succeeds with valid data', async () => {
+    const user = await User.findOne()
     await api
       .post('/api/blogs')
+      .set(helper.authorizationHeader(user))
       .send(helper.exampleBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -55,8 +64,10 @@ describe('addition of a new note', () => {
     )
   })
   test('has the default value of likes as zero', async () => {
+    const user = await User.findOne()
     const response = await api
       .post('/api/blogs')
+      .set(helper.authorizationHeader(user))
       .send(helper.exampleBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -64,6 +75,7 @@ describe('addition of a new note', () => {
     expect(response.body.likes).toEqual(0)
   })
   test('fails with status code 400 if title is invalid', async () => {
+    const user = await User.findOne()
     const newBlog = {
       author: 'magic-oreo',
       url: 'invalid url'
@@ -71,6 +83,7 @@ describe('addition of a new note', () => {
 
     await api
       .post('/api/blogs')
+      .set(helper.authorizationHeader(user))
       .send(newBlog)
       .expect(400)
 
@@ -82,46 +95,62 @@ describe('addition of a new note', () => {
       author: 'magic-oreo',
       title: 'Some title'
     }
-
+    const user = await User.findOne()
     await api
       .post('/api/blogs')
+      .set(helper.authorizationHeader(user))
       .send(newBlog)
       .expect(400)
 
     const blogsAtEnd = await helper.blogsInDb()
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
   })
+
+  test('fails with status code 401 if the token is invalid', async () => {
+    const newBlog = {
+      author: 'magic-oreo',
+      title: 'Some title'
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401)
+
+    expect(response.text).toContain('invalid token')
+  })
 })
 describe('deletion of a blog', () => {
   test('succeeds with status code 204 if id is valid', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-
+    const user = await User.findOne()
+    const blog = await Blog.create({ ...helper.exampleBlog, user: user._id })
     await api
-      .delete(`/api/blogs/${blogToDelete.id}`)
+      .delete(`/api/blogs/${blog.id}`)
+      .set(helper.authorizationHeader(user))
       .expect(204)
 
     const blogsAtEnd = await helper.blogsInDb()
 
     expect(blogsAtEnd).toHaveLength(
-      helper.initialBlogs.length - 1
+      helper.initialBlogs.length
     )
 
-    const contents = blogsAtEnd.map(r => r.title)
+    const contents = blogsAtEnd.map(r => r.id)
 
-    expect(contents).not.toContain(blogToDelete.title)
+    expect(contents).not.toContain(helper.exampleBlog.id)
   })
 })
 describe('updating a blog', () => {
   test('succeeds if data is valid', async () => {
     const blogsAtStart = await helper.blogsInDb()
     const blogToUpdate = blogsAtStart[0]
-
+    const user = await User.findOne()
     const newBlog = {
       author: 'new title'
     }
     await api
       .put(`/api/blogs/${blogToUpdate.id}`)
+      .set(helper.authorizationHeader(user))
       .send(newBlog)
       .expect(200)
 
