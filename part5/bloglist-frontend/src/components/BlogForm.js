689f230368fef9216320c4a7f8b0ea71e15c0b25
magic-oreo <deh.vud@gmail.com>
2023-03-29 14:41:05 -0500

    5.5: Add BlogForm component

---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [blogTitle, setBlogTitle] = useState('')
+  const [blogAuthor, setBlogAuthor] = useState('')
+  const [blogUrl, setBlogUrl] = useState('')
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: blogTitle,
+      author: blogAuthor,
+      url: blogUrl
+    })
+    setBlogTitle('')
+    setBlogAuthor('')
+    setBlogUrl('')
+    
+  }
+
+  return (
+    <div>
+      <h2>Create a new blog</h2>
+      <form onSubmit={addBlog}>
+      <div>
+        title:
+        <input
+          value={blogTitle}
+          label="title"
+          onChange={({ target }) => setBlogTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+        <input
+          value={blogAuthor}
+          label="author"
+          onChange={({ target }) => setBlogAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          value={blogUrl}
+          label="url"
+          onChange={({ target }) => setBlogUrl(target.value)}
+        />
+      </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogForm
