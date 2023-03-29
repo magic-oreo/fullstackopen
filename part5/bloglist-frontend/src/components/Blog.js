d64135afe052b797889468c18584b903233e0e7a
magic-oreo <deh.vud@gmail.com>
2023-03-29 16:33:31 -0500

    5.7: Implement info toggle for blog entries

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    paddingBottom: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+      {blog.title}, {blog.author}
+      <button onClick={() => setExpanded(!expanded)}>
+        {expanded ? 'hide' : 'view'}
+      </button>
+      </div>
+      {expanded &&
+        <>
+          <a href={blog.url}>{blog.url}</a> <br />
+          {blog.likes} <button>Likes</button> <br />
+          {blog.user.name}
+        </>
+      }
+    </div>
+  )
+}
 
 export default Blog
