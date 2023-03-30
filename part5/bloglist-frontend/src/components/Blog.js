2a829a97742072b1b281cf9a251fc062696b9f54
magic-oreo <deh.vud@gmail.com>
2023-03-30 13:18:32 -0500

    5.9: Increase likes onClick

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
+import blogService from '../services/blogs'
+
 const Blog = ({ blog }) => {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -10,18 +13,25 @@ const Blog = ({ blog }) => {
     marginBottom: 5
   }
 
+  const increaseLikes = () => {
+    blog['likes']++
+    setLikes(blog.likes)
+    blogService
+      .update(blog.id, blog)
+  }
+
   return (
     <div style={blogStyle}>
       <div>
-      {blog.title}, {blog.author}
-      <button onClick={() => setExpanded(!expanded)}>
-        {expanded ? 'hide' : 'view'}
-      </button>
+        {blog.title}, {blog.author}
+        <button onClick={() => setExpanded(!expanded)}>
+          {expanded ? 'hide' : 'view'}
+        </button>
       </div>
       {expanded &&
         <>
           <a href={blog.url}>{blog.url}</a> <br />
-          {blog.likes} <button>Likes</button> <br />
+          {likes} <button onClick={() => increaseLikes()}>Likes</button> <br />
           {blog.user.name}
         </>
       }
