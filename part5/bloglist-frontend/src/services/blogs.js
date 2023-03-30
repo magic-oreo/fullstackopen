d4b5477ccc1c652903b5eba2d9e54ca44a403b41
magic-oreo <deh.vud@gmail.com>
2023-03-30 14:23:08 -0500

    5.11: Implement delete requests

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -25,4 +25,8 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, setToken, update }
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
+}
+
+export default { getAll, create, setToken, update, remove }
